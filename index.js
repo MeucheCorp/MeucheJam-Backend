65fabb1365229931d9591f9799b4276bf44d91c1
MeucheRoume <lucas29.david@gmail.com>
2024-05-05 23:13:41 +0200

    🍄 MeucheCommit 2 !

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // Import dependencies
-const fs = require("fs");
-const { google } = require("googleapis");
-require('dotenv').config();
+const fs = require("fs")
+const { google } = require("googleapis")
+require('dotenv').config()
 
-const service = google.sheets("v4");
+const service = google.sheets("v4")
 
 // Configure auth client
 const authClient = new google.auth.JWT(
@@ -11,14 +11,14 @@ const authClient = new google.auth.JWT(
     null,
     process.env.private_key.replace(/\\n/g, "\n"),
     ["https://www.googleapis.com/auth/spreadsheets"]
-);
+)
 
 const express = require('express')
-const cors = require('cors');
+const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 1677
 
-app.use(cors());
+app.use(cors())
 app.get('/', async (req, res) => {
     res.send(await twitch_channels())
 })
@@ -32,33 +32,37 @@ async function twitch_channels() {
     try {
 
         // Authorize the client
-        const token = await authClient.authorize();
+        const token = await authClient.authorize()
 
         // Set the client credentials
-        authClient.setCredentials(token);
+        authClient.setCredentials(token)
 
         // Get the rows
         const res = await service.spreadsheets.values.get({
             auth: authClient,
             spreadsheetId: "13gAlL6D8nse4vCHOtB54O-MeVYgw5sBrUWjBK7p4bgY",
-            range: "B:B",
-        });
+            range: "C:C",
+        })
 
         // All of the answers
-        const channels = [];
+        const channels = []
 
         // Set rows to equal the rows
-        const rows = res.data.values;
+        const rows = res.data.values
 
         // Check if we have any data and if we do add it to our answers array
         if (rows.length) {
             rows.shift() // Remove the headers
             for (const row of rows) {
-                channels.push(row[0])
+                const channel = row[0]
+                const slash_index = channel.lastIndexOf('/')
+                channels.push(slash_index !== -1
+                    ? channel.substring(slash_index + 1)
+                    : channel
+                )
             }
-
         } else {
-            console.log("No data found.");
+            console.log("No data found.")
         }
 
         return channels
@@ -66,10 +70,10 @@ async function twitch_channels() {
     } catch (error) {
 
         // Log the error
-        console.log(error);
+        console.log(error)
 
         // Exit the process with error
-        process.exit(1);
+        process.exit(1)
 
     }
 
